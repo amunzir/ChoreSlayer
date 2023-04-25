9e0606137c993b54b4d602cb4f6b2c4e9683e869
Aamina Munzir <aaminamunzir123@gmail.com>
2023-04-25 16:18:21 -0500

    first push

--- ChoreSlayer/ViewController.swift
+++ ChoreSlayer/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MonsterName: UILabel!
+    @IBOutlet weak var HealthBar: UIImageView!
+    @IBOutlet weak var HealthLabel: UILabel!
+    @IBOutlet weak var AttackButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
