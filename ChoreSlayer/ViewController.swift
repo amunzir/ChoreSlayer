e7e055658ff7c0dd8cc32f3958a9e84d2473ebf9
Aamina Munzir <aaminamunzir123@gmail.com>
2023-04-25 20:41:59 -0500

    added to viewController file - started attack button - doesn't change health bar yet but decrements health fine

--- ChoreSlayer/ViewController.swift
+++ ChoreSlayer/ViewController.swift
@@ -13,11 +13,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var HealthBar: UIImageView!
     @IBOutlet weak var HealthLabel: UILabel!
     @IBOutlet weak var AttackButton: UIButton!
+    
+    var monsterHealthTotal = 150
+    var monsterName = "Mike W."
+    var healthBarSize = 260
+    var damageDone = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        MonsterName.text = monsterName
+        HealthLabel.text = "\(monsterHealthTotal)/\(monsterHealthTotal)"
     }
 
-
+    @IBAction func AttackPressed(_ sender: UIButton) {
+        damageDone += 5
+        HealthLabel.text = "\(monsterHealthTotal-damageDone)/\(monsterHealthTotal)"
+        let barMultipier = CGFloat(damageDone)/CGFloat(monsterHealthTotal)
+        let newWidth = CGFloat(CGFloat(healthBarSize) * (1.0-barMultipier))
+        print(barMultipier)
+        print(newWidth)
+        /*HealthBar.frame = CGRectMake(
+                     HealthBar.frame.origin.x,
+                     HealthBar.frame.origin.y, newWidth, 50);
+         not quite working*/
+    }
+    
 }
 
